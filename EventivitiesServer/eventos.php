c28b11a1d0c339d076b48b4561c6b868a5d8fa6f
rbrodyr <marcosgallach@gmail.com>
2013-01-02 12:37:26 +0100

    Eventos modificados con los cambios. Añadida duración y hora de inicio

--- EventivitiesServer/eventos.php
+++ EventivitiesServer/eventos.php
@@ -7,7 +7,7 @@ class eventos{
 	public function getEventos(){
 		/*$sql = "SELECT * ";
 		$sql .= " FROM Evento ";	*/	
-		$sql="SELECT Evento.idEvento as idEvento,Nombre,Descripcion,FechaInicio,FechaFin,idLocal,idTipoEvento,Precio,Director,Interpretes,Coalesce( avg( Puntuacion ), 0 ) as media
+		$sql="SELECT Evento.idEvento as idEvento,Nombre,Descripcion,FechaInicio,FechaFin,idLocal,idTipoEvento,Precio,Director,Interpretes,Duracion, HoraInicio, Coalesce( avg( Puntuacion ), 0 ) as media
 		FROM Evento LEFT JOIN Puntuacion on Puntuacion.idEvento=Evento.idEvento 
 		GROUP BY Evento.idEvento";		
 		$db = new bdadministrador();
@@ -17,7 +17,7 @@ class eventos{
 	public function getEventosLocal($idLocal){
 		/*$sql = "SELECT * ";
 		$sql .= " FROM Evento WHERE idLocal=".$idLocal;*/
-		$sql="SELECT Evento.idEvento as idEvento,Nombre,Descripcion,FechaInicio,FechaFin,idLocal,idTipoEvento,Precio,Director,Interpretes,Coalesce( avg( Puntuacion ), 0 ) as media
+		$sql="SELECT Evento.idEvento as idEvento,Nombre,Descripcion,FechaInicio,FechaFin,idLocal,idTipoEvento,Precio,Director,Interpretes,Duracion, HoraInicio, Coalesce( avg( Puntuacion ), 0 ) as media
 		FROM Evento LEFT JOIN Puntuacion on Puntuacion.idEvento=Evento.idEvento
 		GROUP BY Evento.idEvento HAVING idLocal=".$idLocal;	
 		$db = new bdadministrador();
@@ -49,6 +49,8 @@ class eventos{
 				Precio 	double 	No 	0
 				Director 	text 	No
 				Interpretes 	text 	Sí 	NULL
+				Duracion 	int(11) 	Sí 	NULL 	
+				HoraInicio 	time 	Sí 	NULL
 				
 				SELECT Evento.idEvento,Nombre,Descripcion,FechaInicio,FechaFin,idLocal,idTipoEvento,Precio,Director,
 				Interpretes,avg(Puntuacion.Puntuacion) as media
@@ -61,6 +63,7 @@ class eventos{
 						.$obj->Descripcion."\", \"fechaInicio\": \"".$obj->FechaInicio. "\", \"fechaFin\": \"".$obj->FechaFin."\",
 								 \"idTipoEvento\": \"".$obj->idTipoEvento."\", \"precio\": \"".$obj->Precio."\",
 								 \"director\": \"".$obj->Director."\", \"interpretes\": \"".$obj->Interpretes."\",
+								 \"duracion\": \"".$obj->Duracion."\", \"horaInicio\": \"".$obj->HoraInicio."\",								 		
 								 \"media\": \"".$obj->media."\",
 								 \"idLocal\": \"".$obj->idLocal."\" ";
 		
@@ -85,40 +88,8 @@ class eventos{
 		$result = $this->getEventosLocal($idLocal);
 		$json=$this->formateaJSON($result);
 		return $json;	
-	}
+	}	
 	
-	private function insertLibro($libro, $isbn){
-		$sql = sprintf("INSERT INTO libros (libro, isbn) VALUES ('%s', '%s') ",mysql_real_escape_string($libro), mysql_real_escape_string($isbn));
-	
-		$db = new dbmanager();
-		$result = $db->executeQuery($sql);
-		if($result){
-			$idlibro = mysql_insert_id();
-			$sql = "SELECT * FROM libros WHERE id_libro = " . $idlibro;
-			$result = $db->executeQuery($sql);
-			return mysql_fetch_array($result,MYSQL_BOTH);
-		} else {
-			return false;
-		}
-	}
-	
-	public function getJSONInsertLibros($libro, $isbn){
-		$response = array("tag" => "insert", "success" => 0, "error" => 0);
-		$libro = $this->insertLibro($libro, $isbn);
-		if($libro != false){
-			$response["success"] = 1;
-	
-			$response["libro"]["id"] = $libro['id_libro'];
-			$response["libro"]["titulo"] = $libro['libro'];
-			$response["libro"]["isbn"] = $libro['isbn'];
-	
-			return json_encode($response);
-		} else {
-			$response["error"] = 1;
-			$response["error_msg"] = "No se insertó el libro";
-			return json_encode($response);
-		}
-	}
 	
 	public function getJSONError($tag, $errormsg){
 		$response = array("tag" => $tag, "exito" => 0, "error" => 1, "error_msg" => $errormsg);
