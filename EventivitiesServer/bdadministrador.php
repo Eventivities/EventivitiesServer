a995079d541af2180d653f6b1fc10048c304d1ce
rbrodyr <marcosgallach@gmail.com>
2013-01-08 12:53:49 +0100

    Mejoras en bdadministrador y realizado el servicioWeb de registro de usuarios

--- EventivitiesServer/bdadministrador.php
+++ EventivitiesServer/bdadministrador.php
@@ -4,26 +4,30 @@ include_once('globales.php');
 class bdadministrador{
  
  public function executeQuery($sql){
-  $link = mysqli_connect(config::getBBDDServer(), config::getBBDDUser(), config::getBBDDPwd());
-  //$link = mysql_connect(config::getBBDDServer(), config::getBBDDUser(), config::getBBDDPwd());
-  if (!$link)
-    {
-     die('Could not connect: ' . mysql_error());
-    }    
-  
- // mysql_select_db(config::getBBDDName(), $link);
-    $link->select_db(config::getBBDDName());
-    
+  $link = $this->obtenerEnlace();
   //$result = mysql_query($sql);
     $result = $link->query($sql);
+    //	echo mysqli_error($link);
     
-    //echo "$link->affected_rows";
-    
-    
-    
+    //echo "$link->affected_rows";   
   
   $link->close();
   return $result;
  }
+ 
+ public function obtenerEnlace()
+ {
+ 	$link = mysqli_connect(config::getBBDDServer(), config::getBBDDUser(), config::getBBDDPwd());
+ 	//$link = mysql_connect(config::getBBDDServer(), config::getBBDDUser(), config::getBBDDPwd());
+ 	if (!$link)
+ 	{
+ 		die('Could not connect: ' . mysql_error());
+ 	}
+ 	
+ 	// mysql_select_db(config::getBBDDName(), $link);
+ 	$link->select_db(config::getBBDDName());
+ 	return $link; 	
+ }
+ 
 }
 ?>
--- EventivitiesServer/service.registrarusuario.php
+++ EventivitiesServer/service.registrarusuario.php
@@ -1,12 +1,13 @@
 <?php
 include_once('usuarios.php');
-$login = new usuario();
+$usuario = new usuario();
 
-//echo $login->getJSONLogin('al', 'pal');
+
+//echo $usuario->getJSONRegistro('1', '1');
 if(isset($_POST['username']) and (isset($_POST['password']))){
-	$user = $_POST['username'];
-	$pass = $_POST['password'];
-	echo $login->getJSONLogin($user, $pass);
+	$alias = $_POST['username'];
+	$clave = $_POST['password'];
+	echo $login->getJSONRegistro($alias, $clave);
 }
 else{
 	echo $login->getJSONError('envio','Error en el envío de datos');
